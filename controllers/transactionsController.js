4d7b189642617b8feea1416a7eb39a2038763990
Grace Bong <gracebong.park@gmail.com>
2021-06-17 01:06:46 -0400

    controller and model almost done

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -5,25 +5,37 @@ const transactionsArray = require("../models/transactions");
 
 
 transactions.get("/", (req, res) => {
-    res.status(200).json(transactionsArray)
+    res.status(200).json(transactionsArray);
 });
 
-transactions.get("/:id", (req, res) => {
-    const { id } = req.params;
+transactions.get("/:index", (req, res) => {
+    const { index } = req.params;
    
-    transactionsArray[id] ?
-    res.status(200).json(transactionsArray[id]) : res.redirect("/404")
+    transactionsArray[index] ?
+    res.status(200).json(transactionsArray[index]) : res.redirect("/404");
 });
 
 transactions.post("/", (req, res) => {
-    transactionsArray.push(req.body)
-    res.json(transactionsArray[transactionsArray.length - 1])
+    transactionsArray.push(req.body);
+    res.json(transactionsArray[transactionsArray.length - 1]);
 });
 
-transactions.delete("/:id", (req, res) => {
-    
-})
+transactions.delete("/:index", (req, res) => {
+    const { index } = req.params;
 
-// transactions.put()
+    transactionsArray[index] ?
+    res.json(transactionsArray.splice(index, 1)) : res.redirect("/404");
+});
+
+transactions.put("/:index", (req, res) => {
+    const { index } = req.params;
+
+    if (transactionsArray[index]) {
+        transactionsArray[index] = req.body;
+        res.json(transactionsArray[index]);
+    } else {
+        res.redirect("/404");
+    };
+});
 
 module.exports = transactions;
