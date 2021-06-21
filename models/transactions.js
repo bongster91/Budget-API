d329f84cca336af6b3cf9cffa396e45fe7a51645
Grace Bong <gracebong.park@gmail.com>
2021-06-21 19:04:06 -0400

    backend complete

--- models/transactions.js
+++ models/transactions.js
@@ -3,36 +3,36 @@ module.exports = [
         date: "August 14, 1890",
         name: "Voodoo Treatment",
         amount: 50,
-        from: "Random person off the street"
+        memo: "Random person off the street"
     },
     {
         date: "January 20, 2000",
         name: "Self-Love",
         amount: 9000,
-        from: "Might've stole a card..."
+        memo: "Might've stole a card..."
     },
     {
         date: "February 3, 2021",
         name: "Covid-19 Vaccine",
         amount: -450,
-        from: "Got vaccine illegally off dark web"
+        memo: "Got vaccine illegally off dark web"
     },
     {
         date: "December 30, 4000",
         name: "Energy dividend",
         amount: 99999999,
-        from: "Hacked into State Energy Company for the preservation of national integrity and own self-interest"
+        memo: "Hacked into State Energy Company for the preservation of national integrity and own self-interest"
     },
     {
         date: "April 1, 3333",
         name: "Bucks of the Star",
         amount: -1234,
-        from: "Remember when a cup of coffee used to cost $5?"
+        memo: "Remember when a cup of coffee used to cost $5?"
     },
     {
         date: "September 23, 2008",
         name: "Gainss",
         amount: 34978634,
-        from: "What Great Recession?"
+        memo: "What Great Recession?"
     },
 ]
